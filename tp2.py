841b1d111f0c4ab1af24da7239bb4d346ad1b6a1
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2020-08-21 22:54:25 -0300

    informacoes do sistema

--- tp2.py
+++ tp2.py
@@ -4,4 +4,20 @@ import platform
 memoria = psutil.virtual_memory()
 
 capacidade = round(memoria.total/(1024*1024*1024), 2)
-print(capacidade)
+# print(capacidade)
+
+# informações do processador
+processador = platform.processor()
+print(processador)
+
+# informações da rede
+rede = platform.node()
+print(rede)
+
+# detalhes da plataforma
+plataforma = platform.platform()
+print(plataforma)
+
+# sistema operacional
+sistema = platform.system()
+print(sistema)
